1cbec111336e2e3160d8092d78af1f62d8ff34ef
ovideros <14277035+ovideros@user.noreply.gitee.com>
2024-07-17 17:16:11 +0800

    finish pj1EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -2,7 +2,6 @@ package tester;
 
 import static org.junit.Assert.*;
 
-import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
@@ -15,37 +14,31 @@ import student.StudentArrayDeque;
         ArrayDequeSolution<Integer> L2 = new ArrayDequeSolution<>();
         int N = 5000;
         ArrayDequeSolution<String> message = new ArrayDequeSolution<>();
-        int messageSize = 0;
         for (int i = 0; i < N; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
             int randVal = StdRandom.uniform(0, 100);
             if (numberBetweenZeroAndOne < 0.25) {
                 L.addFirst(randVal);
                 L2.addFirst(randVal);
-                appendHelper(messageSize, message, 0, randVal);
-                messageSize += 1;
+                appendHelper( message, 0, randVal);
                 assertEquals(messageToStr(message),L.get(0), L2.get(0));
             } else if (numberBetweenZeroAndOne < 0.5) {
                 L.addLast(randVal);
                 L2.addLast(randVal);
-                appendHelper(messageSize, message, 1, randVal);
-                messageSize += 1;
+                appendHelper( message, 1, randVal);
                 assertEquals(messageToStr(message),L.get(L.size() - 1), L2.get(L2.size() - 1));
             } else if (numberBetweenZeroAndOne < 0.75 && !L.isEmpty() && !L2.isEmpty()) {
-                appendHelper(messageSize, message, 2, randVal);
-                messageSize += 1;
+                appendHelper(message, 2, randVal);
                 assertEquals(messageToStr(message),L.removeFirst(), L2.removeFirst());
             } else if (!L.isEmpty() && !L2.isEmpty()){
-                appendHelper(messageSize, message, 3, randVal);
-                messageSize += 1;
+                appendHelper(message, 3, randVal);
                 assertEquals(messageToStr(message),L.removeLast(), L2.removeLast());
             }
 
         }
     }
 
-    /** True means <=3; false means > 3.*/
-    private void appendHelper(int mS,ArrayDequeSolution<String> m, int type, int rV) {
+    private void appendHelper(ArrayDequeSolution<String> m, int type, int rV) {
         switch (type) {
             case 0:
                 m.addLast("addFirst(" + rV + ")\n");
@@ -60,13 +53,11 @@ import student.StudentArrayDeque;
                 m.addLast("removeLast()\n");
                 break;
         }
-        if (mS > 2) {
-            m.removeFirst();
-        }
     }
 
     private String messageToStr(ArrayDequeSolution<String> m) {
         StringBuilder str = new StringBuilder();
+        str.append("\n");
 
         for (int i = 0; i < m.size(); i++) {
             str.append(m.get(i));
